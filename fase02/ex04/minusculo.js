baa3ed9f4d886bfcd977de5ced47e8999166330b
Giannini99 <59850598+Giannini99@users.noreply.github.com>
2020-01-22 20:41:07 -0300

    5 Fase dia 3

--- fase02/ex04/minusculo.js
+++ fase02/ex04/minusculo.js
@@ -1,7 +1,5 @@
 function minusculo(palavra) {
     var M = palavra.toLowerCase()
     var N = M.substring(0,1)
-    var O = palavra.substring(1,palavra.length)
-    return N+O
-    
+    return N
 }
