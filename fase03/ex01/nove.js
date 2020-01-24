a8f279930014aee07201f909dda1ee383d234942
Giannini99 <59850598+Giannini99@users.noreply.github.com>
2020-01-24 19:50:21 -0300

    2 Fase dia 4

---
+++ fase03/ex01/nove.js
@@ -0,0 +1,11 @@
+function nove(numero)   {
+    var n1 = numero.toString();
+    var n2 = n1.substring(n1.length-1,n1.length);
+    
+    if (n2==="9")
+    return "true"
+    else 
+    return "false"
+    
+}
+
