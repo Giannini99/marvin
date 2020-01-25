a0b84c7c40f571911ddb556dfaa96e81ee14fcaf
Giannini99 <59850598+Giannini99@users.noreply.github.com>
2020-01-24 21:06:23 -0300

    4 Fase dia 4

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,25 @@
+function vogalOuConsoante(letra)    {
+    if (letra=="A")
+    return "Vogal"
+    else if (letra=="a")
+    return "Vogal"
+    else if (letra=="E")
+    return "Vogal"
+    else if (letra=="e")
+    return "Vogal"
+    else if (letra=="I")
+    return "Vogal"
+    else if (letra=="i")
+    return "Vogal"
+    else if (letra=="O")
+    return "Vogal"
+    else if (letra=="o")
+    return "Vogal"
+    else if (letra=="U")
+    return "Vogal"
+    else if (letra=="U")
+    return "Vogal"
+    else
+    return "Consoante"
+
+}
