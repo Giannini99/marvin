6cc1774cc5a2531009f56f5263b44a419d568898
Giannini99 <59850598+Giannini99@users.noreply.github.com>
2020-01-29 21:57:22 -0300

    2 Fase dia 5

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,17 @@
+function ateZero(numero)     {
+    var ZERO = 0
+    if(numero>=ZERO) {
+        while (numero>=ZERO) {
+            console.log(ZERO)
+            ZERO++
+        }
+    } else {
+        while (numero<=ZERO) {
+            console.log(numero)
+            numero++
+        }  
+    }
+
+    
+}
+
